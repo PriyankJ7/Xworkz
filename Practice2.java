377706cb073486c51ce35cb45f992ce2fc5dc63d
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-18 16:39:38 +0530

    Practice program 2

---
+++ Practice2.java
@@ -0,0 +1,28 @@
+class Practice2
+{
+public static void main(String[] args)
+{
+System.out.println("5 methods");
+firstmethod();
+secondmethod();
+thirdmethod();
+fourthmethod();
+fifthmethod();
+}
+public static void firstmethod()
+{
+}
+public static void secondmethod()
+{
+}
+public static void thirdmethod()
+{	
+}
+public static void fourthmethod()
+{
+}
+public static void fifthmethod()
+{
+}
+
+}
