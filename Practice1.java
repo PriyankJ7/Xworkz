4671381f29b917261ddb935983492110ad42ae2f
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-18 16:38:55 +0530

    Practice program 1

---
+++ Practice1.java
@@ -0,0 +1,18 @@
+class Practice1
+{
+public static void main(String[] args)
+{
+System.out.println(" java practise question");
+music();//
+drinking();
+}
+public static void music()
+{	
+System.out.println(" Music is very nice");
+}
+public static void drinking()
+{	
+System.out.println(" Coffee is  nice");
+}
+}
+
