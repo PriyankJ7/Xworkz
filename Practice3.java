075038c5bc785b7f400e84e611f3c69e4381c707
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-18 16:40:26 +0530

    Practice program 3

---
+++ Practice3.java
@@ -0,0 +1,15 @@
+class Practice3
+{
+public static void main(String[] param)
+{
+System.out.println(" program");
+
+write();
+write();
+write();
+}
+public static void write()
+{
+System.out.println("write method invoked 3 times");	
+}
+}
