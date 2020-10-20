f4feb94ec3b80bdfdb6094bbff45a5fdaf5c0f84
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-20 17:13:57 +0530

    Final Method Program

---
+++ FinalMethod.java
@@ -0,0 +1,15 @@
+public class FinalMethod
+{
+public static void main(String[] args)
+{
+final String AdharNo="544637464737"; 
+final String PhoneNo="9986474747";
+final String Email="priyank@gmail.com";
+
+System.out.println("Adhar Number :"+AdharNo);
+System.out.println("Phone Number :"+PhoneNo);
+System.out.println("Email Id :"+Email);
+
+}
+
+}
