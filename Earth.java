b980571cbca9ef5b5bb68fef98aad70ba4a51b1f
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-20 17:12:49 +0530

    Method program

---
+++ Earth.java
@@ -0,0 +1,14 @@
+public class Earth
+{
+public static void main(String[] args)
+{
+statement("Welcome");
+}
+
+public static void statement(String param)
+{
+	System.out.println(""+param);
+
+}
+
+}
