8fd972780943c2905ab2a41b40b9e54128edfaa4
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-20 11:34:41 +0530

    DataType program 3

---
+++ DataTypes3.java
@@ -0,0 +1,23 @@
+class DataTypes3
+{
+public static void main(String[] args)
+{
+byte colourInRembow= 7;
+short noOfCollegesInDistict=30000;
+int shoppingAmout= 24428009;
+long populationOfHuman =1330000;
+double chargeOfElectrons=601766200;
+boolean fruitHasTeast =true;
+char Y='3';
+String nameOfCompany="google";
+System.out.println(colourInRembow);
+System.out.println(noOfCollegesInDistict);
+System.out.println(shoppingAmout);
+System.out.println(populationOfHuman);
+System.out.println(chargeOfElectrons);
+System.out.println(fruitHasTeast);
+System.out.println(Y);
+System.out.println(nameOfCompany);
+
+}
+}
