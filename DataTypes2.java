ad61880c1490f201963b98f018f03d80bf73796b
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-20 11:33:51 +0530

    DataType Program 2

---
+++ DataTypes2.java
@@ -0,0 +1,23 @@
+class DataTypes2
+{
+public static void main(String[] args)
+{
+byte studentInClass= 70;
+short noOfEmployeesInCompany=20000;
+int incomeOfBusinees= 202044289;
+long populationOfIndia =1352600000;
+double chargeOfNeutron=167000000;
+boolean magnetHasTwoPole=true;
+char D='7';
+String nameOfFlower="rose";
+
+System.out.println(studentInClass);
+System.out.println(noOfEmployeesInCompany);
+System.out.println(incomeOfBusinees);
+System.out.println(populationOfIndia);
+System.out.println(chargeOfNeutron);
+System.out.println(magnetHasTwoPole);
+System.out.println(D);
+System.out.println(nameOfFlower);
+}
+}
