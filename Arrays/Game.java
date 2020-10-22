c61ce61346521534f86da1e9bb91f1fc8ccf9fa1
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-22 21:34:06 +0530

    Array

---
+++ Arrays/Game.java
@@ -0,0 +1,25 @@
+public class Game
+{ 
+public static void main(String[] peoples)
+{
+System.out.println(" Invoked by JVM");
+
+System.out.println("peoples:" +peoples);
+
+
+
+//length
+
+int size=peoples.length;
+
+System.out.println("No of members are :" +size);
+
+//index
+
+
+System.out.println("The index zero is:" +peoples[0]);
+System.out.println("The index one is:" +peoples[1]);
+System.out.println("The index two is:" +peoples[2]);
+}
+
+}
