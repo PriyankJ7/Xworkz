de8d0a41ef246acc7e2571f6c78d08f60cd5f630
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-23 21:58:59 +0530

    call method from other class

---
+++ Arrays/Pubg.java
@@ -0,0 +1,14 @@
+public class Pubg
+{
+public static void supply()
+{
+	System.out.println("method invoked by pub-g class");
+	
+}
+
+public static void enemiesAhead()
+{
+	System.out.println("method invoked by pub-g class");
+}
+
+}
