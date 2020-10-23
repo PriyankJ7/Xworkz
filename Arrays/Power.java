30510ace3c923d6af660530355f96d06c3bbc6be
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-10-23 22:03:22 +0530

    call method from other class

---
+++ Arrays/Power.java
@@ -0,0 +1,12 @@
+public class Power
+{
+	public static void main(String[] box)
+	{
+		System.out.println("method invoked by JVM");
+		
+		Colour.nameOfColours(box[0]);
+		Colour.nameOfColours(box[1]);
+		
+		
+}
+}
