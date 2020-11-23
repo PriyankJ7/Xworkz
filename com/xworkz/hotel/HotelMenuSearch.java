f77ae1215429b2cdc15870fef82dbcc2920d2e99
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-11-23 22:14:41 +0530

    Add files via upload
    Program

---
+++ com/xworkz/hotel/HotelMenuSearch.java
@@ -0,0 +1,11 @@
+package com.xworkz.hotel;
+
+public class HotelMenuSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Total items to search" + Food.totalItems());
+		Food.search("Idli");
+	}
+
+}
