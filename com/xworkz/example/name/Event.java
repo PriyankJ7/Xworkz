4e6cb3e49d0520297f6e90fed1ed48e484aada3b
PriyankJ7 <72928353+PriyankJ7@users.noreply.github.com>
2020-11-23 20:31:57 +0530

    Add files via upload
    Package programs

---
+++ com/xworkz/example/name/Event.java
@@ -0,0 +1,16 @@
+package com.xworkz.example.name;
+
+public class Event {
+
+	public static void main(String[] args) {
+		System.out.println("invoked");
+	
+		}
+	
+	public static void celebrate()
+	{
+		System.out.println("joy");
+		// TODO Auto-generated method stu
+	}
+
+}
---
+++ com/xworkz/example/name/Joy.java
@@ -0,0 +1,17 @@
+package com.xworkz.example.name;
+
+public class Joy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("invoked");
+	}
+	
+	public static void memory()
+	{
+		System.out.println("Photos");
+		
+		
+	}
+
+}
